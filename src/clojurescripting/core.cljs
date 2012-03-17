1c17e28018af9b4a0d295fd147720f85b94457ab
Irakli Gozalishvili <rfobic@gmail.com>
2012-03-16 18:59:13 -0700

    Advance example code a bit in so that we dump "hello world" into page dom.

--- src/clojurescripting/core.cljs
+++ src/clojurescripting/core.cljs
@@ -1,3 +1,17 @@
 (ns clojurescripting.core)
 
-(js/alert "Hello World!")
+(defn set-html
+  "Sets `.innerHTML` of the given tagert element to the give `html`"
+  [target & html]
+  (set! target.innerHTML (apply str html)))
+
+(defn set-text
+  "Sets `.textContent` of the given `tagret`  to the given `text`"
+  [target & text]
+  (set! target.textContent (apply str text)))
+
+;; Ineject "Hello world!" into document body.
+(set-html document.body
+          "<div style='background: black; color: white;'>"
+          "<p>Hello world!</p>"
+          "</div>")
