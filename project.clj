043befbc8b73d7506a6742ad9b2fdc2e26830efe
Irakli Gozalishvili <rfobic@gmail.com>
2012-03-16 17:37:47 -0700

    configure project for cljsbuild.

--- project.clj
+++ project.clj
@@ -2,4 +2,9 @@
   :description "Learning clojurescript"
   :url "http://documentup.com/gozala/clojurescripting/"
   :license { :name "MIT"
-             :url "http://jeditoolkit.com/LICENSE" })
+             :url "http://jeditoolkit.com/LICENSE" }
+  :plugins [[lein-cljsbuild "0.1.2"]]
+  :cljsbuild { :builds [{ :source-path "src"
+                          :compiler { :output-to "lib/app.js"
+                                      :optimizations :whitespace
+                                      :pretty-print true }}]})
